ab6a5cebe1b47c95bda7bbea1ab417755b0bf458
HSK021843 <celestielk@gmail.com>
2023-08-15 19:34:10 +0900

    Update 10810.py
    오류 수정
    수정 내용: 인덱스 번호, 범위 설정 다시함

--- 10810.py
+++ 10810.py
@@ -1,9 +1,10 @@
 N, M = map(int, input().split())
-lst = [0] * N
+lst = [0] * (N + 1)
 
 for _ in range(M):
     A, B, C = map(int, input().split())
-    for i in range(A - 1, B):
+    for i in range(A, B + 1):
         lst[i] = C
 
-print(lst)
+for idx in range(1, len(lst)):
+    print(lst[idx], end = ' ')
