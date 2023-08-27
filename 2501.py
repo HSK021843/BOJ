555e52de1bc1ad8020054d9a7a7a8fcc3c484207
HSK021843 <135344588+HSK021843@users.noreply.github.com>
2023-08-27 21:52:48 +0900

    백준 Level 9(23-08-27)

---
+++ 2501.py
@@ -0,0 +1,15 @@
+N, K = map(int, input().split())
+cnt = 0
+
+for i in range(1, N + 1):
+    test = N % i
+
+    if test == 0:
+        cnt += 1
+        
+        if cnt == K:
+            print(i)
+            break
+
+if cnt < K:
+    print(0)
