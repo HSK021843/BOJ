b3e8876984684371da82b4f8cbb9534f4b99fcff
HSK021843 <135344588+HSK021843@users.noreply.github.com>
2023-09-10 02:08:50 +0900

    백준 8단계(23-09-10)

--- Stage 08/11005.py
+++ Stage 08/11005.py
@@ -1,26 +1,16 @@
-import sys
+N, B = map(int, input().split())
+numOrd = []
 
-A, B = map(int, input().split())
-lst = []
+while N > 0:
+    save = N % B
 
-target = 0
-while A >= B:
-    rest = A % B
-    target = A // B
-
-    if rest >= 10:
-        ch = chr(rest + 55)
-        lst.append(ch)
+    if save >= 10:
+        save = chr(save + 55)
+        numOrd.append(save)
     else:
-        lst.append(str(rest))
+        numOrd.append(str(save))
+    
+    N //= B
 
-    A = target
-
-if target >= 10:
-    ch = chr(target + 55)
-    lst.append(ch)
-else:
-    lst.append(str(target))
-
-
-print(''.join(lst))
+for i in range(len(numOrd) - 1, -1, -1):
+    print(numOrd[i], end="")
