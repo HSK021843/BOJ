8e74c93da347c3f00d6b0abd453123ee31b81c39
HSK021843 <135344588+HSK021843@users.noreply.github.com>
2023-09-10 01:54:15 +0900

    Update 10798.py

--- Stage 07/10798.py
+++ Stage 07/10798.py
@@ -1,3 +1,21 @@
+import sys
+
+wordlst = [sys.stdin.readline().strip() for _ in range(5)]
+longest = 0
+
+for word in wordlst:
+    if len(word) > longest:
+        longest = len(word)
+
+for i in range(longest):
+    for j in range(5):
+
+        if i < len(wordlst[j]):
+            sys.stdout.write(wordlst[j][i])
+
+# 원본 코드에서 sys모듈로 input(), print() 대체
+# 원본 코드
+
 wordlst = [input() for _ in range(5)]
 longest = 0
 
