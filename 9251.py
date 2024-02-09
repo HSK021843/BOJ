d15efd89c768de42570c5c345dd792b68c81d614
HSK021843 <135344588+HSK021843@users.noreply.github.com>
2024-02-09 22:45:24 +0900

    Add files via upload

---
+++ 9251.py
@@ -0,0 +1,19 @@
+def LCS(X, Y):
+    n, m = len(X), len(Y)
+    dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if X[i-1] == Y[j-1]:
+                dp[i][j] = dp[i-1][j-1] + 1
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+    return dp[n][m]
+
+
+X = input()
+Y = input()
+
+res = LCS(X, Y)
+print(res)
